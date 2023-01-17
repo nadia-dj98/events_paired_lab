e89891d6003ec3ef78488a5f1e346ea5a7364cd5
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-17 15:41:02 +0000

    Added event listener to enter button and attempted adding new to-do to list

---
+++ script.js
@@ -0,0 +1,21 @@
+const enter = document.querySelector("#enter");
+//method for creating a new <li> element
+const toDoListForm = document.querySelector("#to-do-list-form"); //grabbing the input
+
+//form
+const addToDoItem = (event) => {
+    // event.preventDefault();
+    // const toDoItem = document.createElement("li");
+    // // toDoItem.textContent = event.target.newtodo.value; 
+    // const inputValue = document.getElementById("#newtodo").value;
+    // const toDoList = document.querySelector("#list");
+    // toDoList.appendChild(toDoItem);
+
+    event.preventDefault();
+    const toDoItem = document.createElement("li");
+    toDoItem.textContent = event.target.newItem.value;
+    const toDoList= document.querySelector("#list");
+    toDoList.appendChild(toDoItem);
+} 
+
+toDoListForm.addEventListener('submit', addToDoItem);
